a3d138a94779775adeb655fecdf43acd1fd21637
houseme <housemecn@gmail.com>
2025-07-15 02:09:33 +0800

    improve code

--- rustfs.rb
+++ rustfs.rb
@@ -2,18 +2,6 @@ class Rustfs < Formula
   VERSION = "1.0.0-alpha.17".freeze
   GITHUB_REPO = "rustfs/rustfs".freeze
 
-  BINARY_CONFIGS = {
-    "aarch64-apple-darwin" => "ac6bee72fb24fab611bdc7c427b7c174c86d543d1ec7c58cb7eeb89fe62d671d",
-    "x86_64-apple-darwin" => "8e30fb72a59f0a657c8f4eecde69485596cb83d6eb831e54515a81b5d0b6d071",
-    "aarch64-unknown-linux-musl" => "0c332a1c9f05330ac24598dd29ddc15819c5a5783b8e95ef513a7fa3921675b1",
-    "x86_64-unknown-linux-musl" => "96081fa567496aa95c755cc4ff8e3366adc7f7da9db72525e18a57bf5b44d607"
-  }.freeze
-
-  PLATFORM_MAPPING = {
-    macos: { arm: "aarch64-apple-darwin", intel: "x86_64-apple-darwin" },
-    linux: { arm: "aarch64-unknown-linux-musl", intel: "x86_64-unknown-linux-musl" }
-  }.freeze
-
   desc "High-performance distributed object storage written in Rust"
   homepage "https://rustfs.com"
   url "https://github.com/#{GITHUB_REPO}/archive/refs/tags/#{VERSION}.tar.gz"
@@ -21,17 +9,14 @@ class Rustfs < Formula
   license "Apache-2.0"
   head "https://github.com/#{GITHUB_REPO}.git", branch: "main", shallow: false
 
-  # 标准的构建依赖声明。Homebrew 会在需要时自动处理。
   depends_on "rust" => :build
   depends_on "protobuf" => :build
   depends_on "flatbuffers" => :build
-  depends_on "pkg-config" => :build if OS.linux?
-  depends_on "openssl@3" if OS.linux?
+  depends_on "pkg-config" => :build, on_linux: true
+  depends_on "openssl@3", on_linux: true
 
   def install
-    # 如果二进制可用且 Homebrew 决定使用它 (build.bottle? is true), 则进行二进制安装。
-    # 否则 (包括 --build-from-source, --HEAD, 或无可用 bottle 的情况), 从源码构建。
-    if binary_available? && build.bottle?
+    if binary_available? && build.bottle? && !build.head?
       install_from_binary
     else
       install_from_source
@@ -46,7 +31,7 @@ class Rustfs < Formula
                      else
                        "binary"
                      end
-    platform_info = "#{OS.mac? ? "macOS" : "Linux"} (#{Hardware::CPU.arch})"
+    platform_info = "#{OS.kernel_name} (#{Hardware::CPU.arch})"
 
     s = <<~EOS
       Thank you for installing rustfs!
@@ -62,10 +47,7 @@ class Rustfs < Formula
       rustfs --version
     EOS
 
-    if build.head?
-      s += "\n\nNote: You have installed the latest development version from HEAD."
-    end
-
+    s += "\n\nNote: You have installed the latest development version from HEAD." if build.head?
     s
   end
 
@@ -76,20 +58,43 @@ class Rustfs < Formula
 
   private
 
-  def platform_target
-    @platform_target ||= PLATFORM_MAPPING.dig(OS.mac? ? :macos : :linux, Hardware::CPU.arm? ? :arm : :intel)
+  def rust_target
+    on_macos do
+      return "aarch64-apple-darwin" if Hardware::CPU.arm?
+      return "x86_64-apple-darwin" if Hardware::CPU.intel?
+    end
+    on_linux do
+      return "aarch64-unknown-linux-musl" if Hardware::CPU.arm?
+      return "x86_64-unknown-linux-musl" if Hardware::CPU.intel?
+    end
+    nil
+  end
+
+  def binary_info
+    @binary_info ||= begin
+                       target = rust_target
+                       break [nil, nil] unless target
+
+                       url = "https://github.com/#{GITHUB_REPO}/releases/download/#{VERSION}/rustfs-#{target}.zip"
+                       sha256 = case target
+                                when "aarch64-apple-darwin" then "ac6bee72fb24fab611bdc7c427b7c174c86d543d1ec7c58cb7eeb89fe62d671d"
+                                when "x86_64-apple-darwin" then "8e30fb72a59f0a657c8f4eecde69485596cb83d6eb831e54515a81b5d0b6d071"
+                                when "aarch64-unknown-linux-musl" then "0c332a1c9f05330ac24598dd29ddc15819c5a5783b8e95ef513a7fa3921675b1"
+                                when "x86_64-unknown-linux-musl" then "96081fa567496aa95c755cc4ff8e3366adc7f7da9db72525e18a57bf5b44d607"
+                                else nil
+                                end
+                       [url, sha256]
+                     end
   end
 
   def binary_available?
-    target = platform_target
-    target && BINARY_CONFIGS.key?(target)
+    url, sha256 = binary_info
+    !url.nil? && !sha256.nil?
   end
 
   def install_from_binary
     ohai "Installing from pre-compiled binary..."
-    target = platform_target
-    url = "https://github.com/#{GITHUB_REPO}/releases/download/#{VERSION}/rustfs-#{target}.zip"
-    sha256 = BINARY_CONFIGS[target]
+    url, sha256 = binary_info
 
     resource "binary" do
       url url
@@ -108,11 +113,11 @@ class Rustfs < Formula
     ENV["CARGO_BUILD_JOBS"] = ENV.make_jobs.to_s
     ENV.deparallelize
 
-    target = platform_target
+    target = rust_target
     cargo_args = %w[build --release --bin rustfs]
     install_path = "target/release/rustfs"
 
-    if OS.linux? && target
+    if target
       system "rustup", "target", "add", target
       cargo_args.concat(["--target", target])
       install_path = "target/#{target}/release/rustfs"
